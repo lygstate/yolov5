eac53fda3de3c72ef3ca080a4c33478ad72ab0f0
Yonggang Luo <luoyonggang@gmail.com>
2021-07-17 17:03:47 +0000

    Improve start-shell.bat

--- start-shell.bat
+++ start-shell.bat
@@ -1,2 +1,3 @@
 set PATH=%UserProfile%/.conda/envs/test-env;%UserProfile%/.conda/envs/test-env/Library/mingw-w64/bin;%UserProfile%/.conda/envs/test-env/Library/usr/bin;%UserProfile%/.conda/envs/test-env/Library/bin;%UserProfile%/.conda/envs/test-env/Scripts;%UserProfile%/.conda/envs/test-env/bin;C:/ProgramData/Anaconda3/condabin;${env.PATH}
-conda activate test-env
+call conda activate test-env
+python calculate.py
