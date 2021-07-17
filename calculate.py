f7fd8b4febc36969a650e839d62983db6dd62427
Yonggang Luo <luoyonggang@gmail.com>
2021-07-17 10:03:04 +0800

    Enabling the http server

--- calculate.py
+++ calculate.py
@@ -1,50 +1,73 @@
 import argparse
-from pathlib import Path
-import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 import json
+import socketserver
+from typing import Tuple
 import cv2
 import torch
+import numpy as np
 from numpy import random
 from models.experimental import attempt_load
-from utils.datasets import LoadImages
-from utils.general import check_img_size, non_max_suppression, scale_coords, \
-    strip_optimizer, set_logging
-from utils.plots import plot_one_box
+from utils.datasets import LoadImages, letterbox
+from utils.general import check_img_size, non_max_suppression, scale_coords, set_logging
 from utils.torch_utils import select_device
 
+class Resquest(BaseHTTPRequestHandler):
+    def __init__(self, request: bytes, client_address: Tuple[str, int], server: socketserver.BaseServer) -> None:
+        super().__init__(request, client_address, server)
+        # Initialize
+        set_logging()
+        device = select_device(opt.device)
+        half = device.type != 'cpu'  # half precision only supported on CUDA
+        weights, imgsz = opt.weights, opt.img_size
+        # Load model
+        model = attempt_load(weights, map_location=device)  # load FP32 model
+        imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
+        if half:
+            model.half()  # to FP16
+        # Get names and colors
+        names = model.module.names if hasattr(model, 'module') else model.names
+        colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
-def detect():
-    out, source, weights, imgsz = opt.output, opt.source, opt.weights, opt.img_size
-    if not os.path.exists(out):
-        os.makedirs(out)
+        self.imgsz = imgsz
+        self.model = model
+        self.device = device
+        self.half = half
+        self.names = names
+        self.colors = colors
 
-    # Initialize
-    set_logging()
-    device = select_device(opt.device)
-    half = device.type != 'cpu'  # half precision only supported on CUDA
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        items = self.detect(post_body)
+        self.wfile.write(json.dumps(items).encode())
 
-    # Load model
-    model = attempt_load(weights, map_location=device)  # load FP32 model
-    imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
-    if half:
-        model.half()  # to FP16
+    def loadImage(self, buf):
+        img0 = cv2.imdecode(buf)
 
-    # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+        # Padded resize
+        img = letterbox(img0, new_shape=self.imgsz)[0]
 
-    # 加载四张图片
-    dataset = LoadImages(source, img_size=imgsz)
+        # Convert
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+        return img, img0
 
-    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
-    _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
-    
-    #****************************************************************
-    four_pic=[]#存放4张图的结果
-    pic_index = 0
-    #****************************************************************
-    for p, img, im0s, _ in dataset:  #dataset里保存四个图像
+    def detect(self, buf):
+        img, img0 = self.loadImage(buf)
+        print(f'Handing {img0.shape}')
+        device = self.device
+        half = self.half
+        model = self.model
+        imgsz = self.imgsz
+
+        img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+        _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
+
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -58,11 +81,10 @@ def detect():
         pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
         items = []
         # Process detections
-        im0 = im0s
         for _, det in enumerate(pred):  # detections per image
             if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                # Rescale boxes from img_size to img0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
                 for *xyxy, conf, cls in reversed(det):                                  
                     xyxy = torch.tensor(xyxy).numpy()
                     conf = conf.cpu().numpy().item()
@@ -73,31 +95,12 @@ def detect():
                         'x1': int(xyxy[2]),
                         'y1': int(xyxy[3]),
                         'conf': conf,
-                        'label': names[cls],
+                        'label': self.names[cls],
                         'cls': cls
                     })
-        print('\n')
-        for item in items:
-            cls = item['cls']
-            conf = item['conf']
-            label = item['label']
-            color=colors[cls]
-            xyxy = [item['x0'], item['y0'], item['x1'], item['y1']]
-            #if False:
-            if cls > 35 and conf > 0.6:
-                cropped_image = im0[item['y0']-2:item['y1']+2, item['x0']-2:item['x1']+2]
-                crop_p = f"{out}/{pic_index}_{item['x0']}_{item['y0']}_{item['x1']}_{item['y1']}_{cls}_{label}.bmp"
-                # cv2.imwrite(crop_p, cropped_image)
-            plot_one_box(xyxy, im0, label=label, color=color, line_thickness=1)
-        four_pic.append(items)
-        pic_index +=1
-        # cv2.imshow(p, im0)
-        name = Path(p).name
-        cv2.imwrite(f'{out}/{name}', im0)
+        return items
 
-    with open(f'{out}/detected.json', 'w') as f:
-        json.dump(four_pic, f, indent=2)
-    cv2.waitKey()
+host = ('localhost', 8888)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -122,11 +125,6 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     print(opt)
 
-    with torch.no_grad():
-        if opt.update:  # update all models (to fix SourceChangeWarning)
-            for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
-                detect()
-                strip_optimizer(opt.weights)
-        else:
-            detect()
-
+    server = HTTPServer(host, Resquest)
+    print("Starting server, listen at: %s:%s" % host)
+    server.serve_forever()
